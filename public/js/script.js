75cd48efe49e352b073bf870e03c496b5c442633
cratermaik <cratermaik@gmail.com>
2020-12-22 18:56:38 -0500

    Update Scroll top Chat box

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@
   document.getElementById('enviar_MSG').submit();
   
 } */
+
 $(function() {
   let $window = $(window);
   let avatarURL = $(".img-url").attr("src");
@@ -10,15 +11,43 @@ $(function() {
   const socket = io();
   $inputMessage = $('.inputMSG')
   $inputMessage.focus();
+  $messages = $('.messages');
 
+  
   socket.on("new message", (data) => {
     msgTemplate(data)
-
+ 
   });
   function clearInput(msg){
     return $('<div/>').text(msg).text();
   }
 
+  function addMessageElement (el, options) {
+    var $el = $(el);
+
+    if(!options) {
+      options = {};
+    }
+    if(typeof options.fade === 'undefined') {
+      options.fade = true;
+    }
+    if(typeof options.prepend === 'undefined'){
+      options.prepend = false;
+    }
+
+    if(options.fade) {
+      $el.hide().fadeIn(120);
+
+    }
+    if(options.prepend){
+      $messages.prepend($el);
+    } else {
+      $messages.append($el);
+    }
+
+    $messages[0].scrollTop = $messages[0].scrollHeight;
+
+  }
   function addMessage() {
    
     let addMgs = $inputMessage.val();
@@ -32,59 +61,62 @@ $(function() {
         username: userName
       }
       renderMessage(data)
+      
       idSave = userName;
       socket.emit('add message', data)
+
     }
 
-
   }
-  function renderMessage (data) {
-    let boxMessage = document.querySelector('#boxChat');
-    let templateMSG = `
-          <li class="collection-item avatar">
-            <img src="${avatarURL}" alt="" class="circle">
-            <span class="title" style="color: white">${userName}</span>
-            <p>${data.content}</p>
-              
-          </li>
-        `;
-    boxMessage.innerHTML += templateMSG;
+  function renderMessage (data, options) {
 
+    options = options || {};
+    let $messageDiv = '';
+
+    $divUser = $(`<span class="title" style="color: white">${userName}</span><p>${data.content}</p>`)
+
+    $imgAvatar = $(`<img src="${avatarURL}" alt="${userName}-avatar" class="circle">`);
+
+    $messageDiv = $('<li class="collection-item avatar"/>')
+      .append($imgAvatar, $divUser);
+
+    addMessageElement($messageDiv, options);
+    
   }
  
   function msgTemplate(data) {
     let boxMessage = document.querySelector('#boxChat');
 
-    let template
+
+    $divUser = $(`<span class="title" style="color: ${data.colorName}">${data.author} <span class="datep">${data.date}</span></span><p>${data.content}</p>`)
+
+    $divUserTwo = $(`<p>${data.content}</p>`)
+
+    $imgAvatar = $(`<img src="${data.avatarURL}" alt="${data.author}-avatar" class="circle">`);
+
     if (idSave === data.id) {
       
-      template = `
-          <li class="collection-item item-chat">
-            <p>${data.content}</p>
-          </li>
-          `;
+       $messageDiv = $('<li class="collection-item item-chat" id="msg-el" />')
+         .append($divUserTwo);
+
     } else {
       idSave = data.id
-      template = `
-          <li class="collection-item avatar">
-            <img src="${data.avatarURL}" alt="" class="circle">
-            <span class="title" style="color: ${data.colorName}">${data.author} <span class="datep">${data.date}</span></span>
-            <p>${data.content}</p>
-              
-          </li>
-        `;
+
+      $messageDiv = $('<li class="collection-item avatar"/>')
+        .append($imgAvatar, $divUser);
+
+     
     }
-
-
-    boxMessage.innerHTML += template;
+    addMessageElement($messageDiv);
   }
+
   $window.keydown(function (event) {
     if(!(event.ctrlKey || event.metaKey || event.altKey)) {
       $inputMessage.focus();
     }
     if(event.which === 13) {
       addMessage();
-
+     
     }
   })
 
