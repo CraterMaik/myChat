c3a93ad3c1d087d595cc30ee59ccd5e4922b1882
AndreMor955 <moralesandre75@gmail.com>
2021-01-17 16:42:57 -0500

    Fechas en mensajes + cambio forma obtener avatar

--- rutas/index.js
+++ rutas/index.js
@@ -5,22 +5,27 @@ const CheckAuth = require('../auth');
 const { processFrontEndMessage } = require("../renderMessage.js");
 
 router.get('/', CheckAuth, async function (req, res) {
-  const avatarURL = `https://cdn.discordapp.com${req.user.avatar ? '' : '/embed'}/avatars${req.user.avatar ? "/" + req.user.id : ""}/${req.user.avatar || req.user.discriminator % 5}.${req.user.avatar && req.user.avatar.startsWith("a_") ? "gif" : "png"}`;
-  const channel = await req.client.channels.fetch(process.env.ID_CHANNEL);
-  if (!channel) return res.status(500).send("Canal inválido.<br>Por favor corriga ID_CHANNEL con la ID del canal correcta.");
-  const pre_messages = await channel.messages.fetch();
-  const lastMessage = JSON.stringify({ author: pre_messages.filter(e => Boolean(e.content || e.attachments.first())).first().author.username, id: pre_messages.filter(e => Boolean(e.content || e.attachments.first())).first().id });
-  const messages = pre_messages.filter(e => Boolean(e.content || e.attachments.first())).map(processFrontEndMessage.bind(null, req.client)).reverse();
-  const key = req.csrfToken();
-  someKeys.set(key, req.user.id);
-  res.render("index.ejs", {
-    user: req.user,
-    avatarURL: avatarURL,
-    channel: channel,
-    messages,
-    key,
-    lastMessage
-  });
+  try {
+    const user = await req.client.users.fetch(req.user.id);
+    const avatarURL = user.displayAvatarURL({ format: "png", dynamic: true, size: 1024 });
+    const channel = await req.client.channels.fetch(process.env.ID_CHANNEL).catch(() => { });
+    if (!channel) return res.status(500).send("Canal inválido.<br>Por favor corriga ID_CHANNEL con la ID del canal correcta.");
+    const pre_messages = await channel.messages.fetch();
+    const lastMessage = JSON.stringify({ author: pre_messages.filter(e => Boolean(e.content || e.attachments.first())).first().author.username, id: pre_messages.filter(e => Boolean(e.content || e.attachments.first())).first().id });
+    const messages = pre_messages.filter(e => Boolean(e.content || e.attachments.first())).map(processFrontEndMessage.bind(null, req.client)).reverse();
+    const key = req.csrfToken();
+    someKeys.set(key, req.user.id);
+    res.render("index.ejs", {
+      user: req.user,
+      avatarURL,
+      channel,
+      messages,
+      key,
+      lastMessage
+    });
+  } catch (err) {
+    res.status(500).send("Un error ocurrió!<br><br>" + err);
+  }
 }).get('/login', function (req, res, next) {
   if (req.query.error === "access_denied") return res.status(401).send("Primero debe iniciar sesión en Discord.<br><a href='/'>Click</a> para volver a intentarlo");
   else {
