88ab8853b9589ff49df65118e933979c3278b3a9
Eduardo Espinoza <eduherere@hotmail.com>
2021-04-12 16:46:19 -0500

    Prettier

--- index.js
+++ index.js
@@ -1,178 +1,210 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const passport = require('passport')
-const { Strategy } = require('passport-discord')
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const { Strategy } = require('passport-discord');
 
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-const session = require('express-session')
-const csurf = require('csurf')
+const session = require('express-session');
+const csurf = require('csurf');
 global.someKeys = new Map();
 
-const path = require('path')
-const Discord = require('discord.js')
-const client = new Discord.Client({ allowedMentions: { parse: [] } })
-const { processFrontEndMessage, validInvs } = require("./renderMessage.js");
+const path = require('path');
+const Discord = require('discord.js');
+const client = new Discord.Client({ allowedMentions: { parse: [] } });
+const { processFrontEndMessage, validInvs } = require('./renderMessage.js');
 
-const webhook = new Discord.WebhookClient(process.env.ID_WH, process.env.TOKEN_WH, { allowedMentions: { parse: [] } });
+const webhook = new Discord.WebhookClient(
+    process.env.ID_WH,
+    process.env.TOKEN_WH,
+    { allowedMentions: { parse: [] } }
+);
 
 passport.serializeUser((user, done) => {
-  done(null, user)
-})
-
-passport.deserializeUser((obj, done) => {
-  done(null, obj)
-})
-
-const scopes = ['identify']
-
-passport.use(new Strategy({
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: `${process.env.URL}/login`,
-  scope: scopes
-}, function (accessToken, refreshToken, profile, done) {
-  process.nextTick(function () {
-    return done(null, profile)
-  })
-}))
-
-app
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .engine("html", require('ejs').renderFile)
-  .use(express.static(path.join(__dirname, "/public")))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs")
-  .use(session({
-    secret: 'name',
-    resave: false,
-    saveUninitialized: false
-  }))
-  .use(passport.initialize())
-  .use(passport.session())
-  .use(csurf({ cookie: false }))
-  .use(function (req, res, next) {
-    req.client = client
-    next()
-  })
-  .use("/", require('./rutas/index'))
-
-  .get('*', function (req, res) { res.status(404).sendFile(__dirname + '/views/404.html') })
-  .use('*', function (req, res) { res.status(405).sendFile(__dirname + '/views/405.html') });
-
-client.on('ready', async () => {
-  console.log('Bot ready!');
-})
-
-io.on('connection', socket => {
-  socket.on('add message', async function (key, pre_content) {
-    if (!someKeys.has(key)) return;
-    const channel = await client.channels.fetch(process.env.ID_CHANNEL);
-    const user = await client.users.fetch(someKeys.get(key).userID);
-    const member = await channel.guild.members.fetch(someKeys.get(key).userID).catch(() => { });
-    if (someKeys.get(key).send_on && channel.rateLimitPerUser) {
-      const time = someKeys.get(key).send_on;
-      const dif = Date.now() - time.getTime();
-      const seconds = Math.floor(Math.abs(dif / 1000));
-      if(channel.rateLimitPerUser > seconds) return socket.emit("cooldown", seconds, channel.rateLimitPerUser);
-    }
-    someKeys.get(key).send_on = new Date();
-    let content = pre_content;
-    if (validInvs(content)) {
-      content = `**${user.username}** invalid link.`
-    }
-    webhook.send(content, {
-      username: member ? member.displayName : user.username,
-      avatarURL: user.displayAvatarURL({ format: "png" })
-    }).then(() => {
-      socket.emit("cooldown", null, channel.rateLimitPerUser)
-    })
-  })
-
-  socket.on('join', async function (key) {
-    if (!someKeys.has(key)) return;
-    const channel = await client.channels.fetch(process.env.ID_CHANNEL_LOG);
-    const user = await client.users.fetch(someKeys.get(key).userID);
-    webhook.send(`**Join:** ${user.username}#${user.discriminator} (${user.id})`, {
-      username: "MyChat",
-      avatarURL: 'https://i.imgur.com/TVaNWMn.png'
-    });
-
-    socket.userId = someKeys.get(key).userID;
-    socket.key = key;
-    channel.send({
-      embed: {
-        title: `Join: ${user.username}#${user.discriminator} (${user.id})`,
-        color: 0x8db600
-      }
-    })
-
-  })
-
-  socket.on('disconnect', async function () {
-    const user = await client.users.fetch(socket.userId).catch(() => { });
-    if (!user) return;
-    const channel = await client.channels.fetch(process.env.ID_CHANNEL_LOG);
-    webhook.send(`**Leave:** ${user.username}#${user.discriminator} (${user.id})`, {
-      username: 'MyChat',
-      avatarURL: 'https://i.imgur.com/TVaNWMn.png'
-    });
-
-    channel.send({
-      embed: {
-        title: `Leave: ${user.username}#${user.discriminator} (${user.id})`,
-        color: 0xe52b50
-      }
-    });
-  })
+    done(null, user);
 });
 
-client.on('message', async message => {
-  if (!message.content && !message.attachments.first()) return;
-  if (message.channel.id !== process.env.ID_CHANNEL) return;
-  const dataMSG = processFrontEndMessage(client, message);
-  io.emit('new message', dataMSG);
-})
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+const scopes = ['identify'];
+
+passport.use(
+    new Strategy(
+        {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: `${process.env.URL}/login`,
+            scope: scopes,
+        },
+        function (accessToken, refreshToken, profile, done) {
+            process.nextTick(function () {
+                return done(null, profile);
+            });
+        }
+    )
+);
+
+app.use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .engine('html', require('ejs').renderFile)
+    .use(express.static(path.join(__dirname, '/public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .use(
+        session({
+            secret: 'name',
+            resave: false,
+            saveUninitialized: false,
+        })
+    )
+    .use(passport.initialize())
+    .use(passport.session())
+    .use(csurf({ cookie: false }))
+    .use(function (req, res, next) {
+        req.client = client;
+        next();
+    })
+    .use('/', require('./rutas/index'))
+
+    .get('*', function (req, res) {
+        res.status(404).sendFile(__dirname + '/views/404.html');
+    })
+    .use('*', function (req, res) {
+        res.status(405).sendFile(__dirname + '/views/405.html');
+    });
+
+client.on('ready', async () => {
+    console.log('Bot ready!');
+});
+
+io.on('connection', (socket) => {
+    socket.on('add message', async function (key, pre_content) {
+        if (!someKeys.has(key)) return;
+        const channel = await client.channels.fetch(process.env.ID_CHANNEL);
+        const user = await client.users.fetch(someKeys.get(key).userID);
+        const member = await channel.guild.members
+            .fetch(someKeys.get(key).userID)
+            .catch(() => {});
+        if (someKeys.get(key).send_on && channel.rateLimitPerUser) {
+            const time = someKeys.get(key).send_on;
+            const dif = Date.now() - time.getTime();
+            const seconds = Math.floor(Math.abs(dif / 1000));
+            if (channel.rateLimitPerUser > seconds)
+                return socket.emit(
+                    'cooldown',
+                    seconds,
+                    channel.rateLimitPerUser
+                );
+        }
+        someKeys.get(key).send_on = new Date();
+        let content = pre_content;
+        if (validInvs(content)) {
+            content = `**${user.username}** invalid link.`;
+        }
+        webhook
+            .send(content, {
+                username: member ? member.displayName : user.username,
+                avatarURL: user.displayAvatarURL({ format: 'png' }),
+            })
+            .then(() => {
+                socket.emit('cooldown', null, channel.rateLimitPerUser);
+            });
+    });
+
+    socket.on('join', async function (key) {
+        if (!someKeys.has(key)) return;
+        const channel = await client.channels.fetch(process.env.ID_CHANNEL_LOG);
+        const user = await client.users.fetch(someKeys.get(key).userID);
+        webhook.send(
+            `**Join:** ${user.username}#${user.discriminator} (${user.id})`,
+            {
+                username: 'MyChat',
+                avatarURL: 'https://i.imgur.com/TVaNWMn.png',
+            }
+        );
+
+        socket.userId = someKeys.get(key).userID;
+        socket.key = key;
+        channel.send({
+            embed: {
+                title: `Join: ${user.username}#${user.discriminator} (${user.id})`,
+                color: 0x8db600,
+            },
+        });
+    });
+
+    socket.on('disconnect', async function () {
+        const user = await client.users.fetch(socket.userId).catch(() => {});
+        if (!user) return;
+        const channel = await client.channels.fetch(process.env.ID_CHANNEL_LOG);
+        webhook.send(
+            `**Leave:** ${user.username}#${user.discriminator} (${user.id})`,
+            {
+                username: 'MyChat',
+                avatarURL: 'https://i.imgur.com/TVaNWMn.png',
+            }
+        );
+
+        channel.send({
+            embed: {
+                title: `Leave: ${user.username}#${user.discriminator} (${user.id})`,
+                color: 0xe52b50,
+            },
+        });
+    });
+});
+
+client.on('message', async (message) => {
+    if (!message.content && !message.attachments.first()) return;
+    if (message.channel.id !== process.env.ID_CHANNEL) return;
+    const dataMSG = processFrontEndMessage(client, message);
+    io.emit('new message', dataMSG);
+});
 
 client.on('typingStart', async (channel, user) => {
-  if (channel.id !== process.env.ID_CHANNEL) return;
-  if (user.bot) return;
+    if (channel.id !== process.env.ID_CHANNEL) return;
+    if (user.bot) return;
 
-  io.emit('typingStart', {
-    channel: {
-      id: channel.id,
-    },
-    user: {
-      id: user.id,
-      tag: `${user.username}#${user.discriminator}`,
-      username: user.username,
-      avatarURL: user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }),
-    }
-  })
-})
+    io.emit('typingStart', {
+        channel: {
+            id: channel.id,
+        },
+        user: {
+            id: user.id,
+            tag: `${user.username}#${user.discriminator}`,
+            username: user.username,
+            avatarURL: user.displayAvatarURL({
+                format: 'png',
+                dynamic: true,
+                size: 1024,
+            }),
+        },
+    });
+});
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 server.listen(port, function () {
-  //login leerá desde DISCORD_TOKEN
-  client.login().then(() => {
-    process.env.BLACKLIST = "810963781232361512"
-})
-  console.log(`Ready, port ${port}`)
-})
+    //login leerá desde DISCORD_TOKEN
+    client.login().then(() => {
+        process.env.BLACKLIST = '810963781232361512';
+    });
+    console.log(`Ready, port ${port}`);
+});
 
-process.on("unhandledRejection", (r) => {
-  console.dir(r)
-})
+process.on('unhandledRejection', (r) => {
+    console.dir(r);
+});
 
-process.on("uncaughtException", (e) => {
-  // Siempre se debe cerrar cuando hay una excepción sin capturar
-  // https://nodejs.org/dist/latest-v12.x/docs/api/process.html#process_warning_using_uncaughtexception_correctly
-  console.dir(e)
-  client.destroy()
-  process.exit(1)
-})
+process.on('uncaughtException', (e) => {
+    // Siempre se debe cerrar cuando hay una excepción sin capturar
+    // https://nodejs.org/dist/latest-v12.x/docs/api/process.html#process_warning_using_uncaughtexception_correctly
+    console.dir(e);
+    client.destroy();
+    process.exit(1);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,191 +1,236 @@
 $(function () {
-  window.addEventListener("pageshow", function (event) {
-    const historyTraversal = event.persisted || (typeof performance != "undefined" && performance.navigation.type === 2);
-    if (historyTraversal) {
-      location.reload();
-    }
-  });
-  const key = document.querySelector('meta[name="app-token"]').getAttribute('content');
-  let $window = $(window)
-  let typingArea = $(".typingArea")
-  const cooldownArea = document.getElementById("cooldownArea");
-  let idSaveLocal = ''
-  let lastMessage = JSON.parse(document.querySelector('meta[name="first-last-message"]').getAttribute('content'));
-
-  const socket = io()
-
-  $inputMessage = $('.inputMSG')
-  $inputMessage.focus()
-  $messages = $('.messages')
-
-  socket.emit('join', key)
-  let md = window.markdownit().use(window.markdownitEmoji)
-  md.renderer.rules.emoji = function (token, idx) {
-    return `<i class="twa twa-3x twa-${token[idx].markup}"></i>`
-  }
-
-  function extractContent(html) {
-    if (html.replace(/<[^>]+>/g, '').trim()) {
-      return true
-    } else {
-      return false
-    }
-  }
-
-  function emoji_animated() {
-    $('.d-emoji-animated').each(function (index, element) {
-      element.src = element.src.replace('.png', '.gif')
-    })
-  }
-
-  function emoji_only() {
-    $('.messages li:last-child p .d-emoji').each(function (e) {
-      $(this).addClass("d-emoji-only")
-    })
-  }
-
-  function getTypingIndicatorIndex(text = '') {
-    return text.indexOf('está') !== -1 ? text.indexOf('está') : (text.indexOf('están') !== -1 ? text.indexOf('están') : 0)
-  }
-
-  socket.on("typingStart", (data) => {
-    const currentText = typingArea.text()
-    const typingIndicatorIndex = getTypingIndicatorIndex(currentText);
-    typingArea.text(currentText.length > 1 ?
-      currentText.replace(/está(?:n?)/gm, `, ${data.user.username} están`)/* ([currentText.slice(typingIndicatorIndex, typingIndicatorIndex-2), `, ${data.user.username}`, currentText.slice(typingIndicatorIndex-2)].join('')) */ :
-      `${data.user.username} está escribiendo...`)
-
-    setTimeout(() => typingArea.text(
-      currentText.replace(`${data.user.username}`, ' ')
-    ), 4000)
-  })
-  socket.on("new message", (data) => {
-    msgTemplate(data)
-    emoji_animated()
-
-    if (!extractContent(data.content)) {
-      emoji_only()
-    }
-  })
-
-  socket.on('add message', function (data) {
-    renderMessage(data)
-  });
-  socket.on("cooldown", async function (remaining, cooldown) {
-    if(!cooldown) return;
-    if(!remaining) {
-      cooldownArea.innerText = moment.duration(cooldown, "seconds").format("h:mm:ss", { trim: false });
-      let e = Number(cooldown);
-      var interval = setInterval(() => {
-        if(e == 0) {
-          cooldownArea.innerText = "El modo lento está activado";
-          return clearInterval(interval);
+    window.addEventListener('pageshow', function (event) {
+        const historyTraversal =
+            event.persisted ||
+            (typeof performance != 'undefined' &&
+                performance.navigation.type === 2);
+        if (historyTraversal) {
+            location.reload();
         }
-        e--;
-        cooldownArea.innerText = moment.duration(e, "seconds").format("h:mm:ss", { trim: false });
-      }, 1000);
-      return;
-    }
-    for(let i = 0; i < 3; i++) {
-      cooldownArea.style.color = "#FF2727";
-      await new Promise((s) => setTimeout(s, 100));
-      cooldownArea.style.color = "#FFFFFF"
-      await new Promise((s) => setTimeout(s, 100));
-    }
-  });
+    });
+    const key = document
+        .querySelector('meta[name="app-token"]')
+        .getAttribute('content');
+    let $window = $(window);
+    let typingArea = $('.typingArea');
+    const cooldownArea = document.getElementById('cooldownArea');
+    let idSaveLocal = '';
+    let lastMessage = JSON.parse(
+        document
+            .querySelector('meta[name="first-last-message"]')
+            .getAttribute('content')
+    );
 
-  function clearInput(msg) {
-    return $('<div/>').text(msg).text()
-  }
+    const socket = io();
 
-  function addMessageElement(el, options) {
-    var $el = $(el)
+    $inputMessage = $('.inputMSG');
+    $inputMessage.focus();
+    $messages = $('.messages');
 
-    if (!options) {
-      options = {}
-    }
-    if (typeof options.fade === 'undefined') {
-      options.fade = true
-    }
-    if (typeof options.prepend === 'undefined') {
-      options.prepend = false
-    }
+    socket.emit('join', key);
+    let md = window.markdownit().use(window.markdownitEmoji);
+    md.renderer.rules.emoji = function (token, idx) {
+        return `<i class="twa twa-3x twa-${token[idx].markup}"></i>`;
+    };
 
-    if (options.fade) {
-      $el.hide().fadeIn(120)
-    }
-    if (options.prepend) {
-      $messages.prepend($el)
-    } else {
-      $messages.append($el)
+    function extractContent(html) {
+        if (html.replace(/<[^>]+>/g, '').trim()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    $messages[0].scrollTop = $messages[0].scrollHeight
-
-  }
-  function addMessage() {
-    let addMgs = $inputMessage.val()
-    addMsg = clearInput(addMgs)
-    if (addMsg) {
-      $inputMessage.val('');
-      socket.emit('add message', key, addMsg);
-    }
-  }
-
-  function renderMessage(data, options) {
-    options = options || {}
-    let $messageDiv = ''
-    $imgAvatar = $(`<img src="${data.avatarURL}" alt="${data.username}-avatar" class="circle">`)
-    $divUserTwo = md.render(data.content)
-    $divUser = $(`<span class="title" style="color: ${data.userColor}">${data.username}</span>
-                ${md.render(data.content)}`)
-
-    if (lastMessage.id === data.id && lastMessage.author === data.author) {
-      $messageDiv = $(`<li id=${data.messageID} class="collection-item item-chat" id="msg-el"/>`)
-        .append($divUserTwo)
-
-    } else {
-      lastMessage.id = data.id;
-      lastMessage.author = data.author;
-      $messageDiv = $(`<li id=${data.messageID} class="collection-item avatar"/>`)
-        .append($imgAvatar, $divUser)
-
+    function emoji_animated() {
+        $('.d-emoji-animated').each(function (index, element) {
+            element.src = element.src.replace('.png', '.gif');
+        });
     }
 
-    addMessageElement($messageDiv, options)
-  }
-
-  function msgTemplate(data) {
-    if (data.attachmentURL) {
-      $divUser = $(`<span class="title" style="color: ${data.colorName}">${data.author} <span class="datep">${data.date}</span></span><p>${data.content}<${data.attachmentURL.endsWith('.mp4') ? 'video controls' : 'img'} class="img-content" src=${data.attachmentURL} /></p>`)
-      $divUserTwo = $(`<p>${data.content}<img class="img-content" src=${data.attachmentURL} /></p>`)
-    } else {
-      $divUser = $(`<span class="title" style="color: ${data.colorName}">${data.author} <span class="datep">${data.date}</span></span><p>${data.content} </p>`)
-      $divUserTwo = $(`<p>${data.content}</p>`)
-    }
-    $imgAvatar = $(`<img src="${data.avatarURL}" alt="${data.author}-avatar" class="circle">`)
-
-    if (lastMessage.id === data.id && lastMessage.author === data.author) {
-      $messageDiv = $(`<li id=${data.messageID} class="collection-item item-chat" id="msg-el" />`)
-        .append($divUserTwo)
-
-    } else {
-      lastMessage.id = data.id;
-      lastMessage.author = data.author;
-      $messageDiv = $(`<li id=${data.messageID} class="collection-item avatar"/>`)
-        .append($imgAvatar, $divUser)
-
+    function emoji_only() {
+        $('.messages li:last-child p .d-emoji').each(function (e) {
+            $(this).addClass('d-emoji-only');
+        });
     }
 
-    addMessageElement($messageDiv)
-  }
+    function getTypingIndicatorIndex(text = '') {
+        return text.indexOf('está') !== -1
+            ? text.indexOf('está')
+            : text.indexOf('están') !== -1
+            ? text.indexOf('están')
+            : 0;
+    }
 
-  $window.keydown(function (event) {
-    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-      $inputMessage.focus()
+    socket.on('typingStart', (data) => {
+        const currentText = typingArea.text();
+        const typingIndicatorIndex = getTypingIndicatorIndex(currentText);
+        typingArea.text(
+            currentText.length > 1
+                ? currentText.replace(
+                      /está(?:n?)/gm,
+                      `, ${data.user.username} están`
+                  ) /* ([currentText.slice(typingIndicatorIndex, typingIndicatorIndex-2), `, ${data.user.username}`, currentText.slice(typingIndicatorIndex-2)].join('')) */
+                : `${data.user.username} está escribiendo...`
+        );
+
+        setTimeout(
+            () =>
+                typingArea.text(
+                    currentText.replace(`${data.user.username}`, ' ')
+                ),
+            4000
+        );
+    });
+    socket.on('new message', (data) => {
+        msgTemplate(data);
+        emoji_animated();
+
+        if (!extractContent(data.content)) {
+            emoji_only();
+        }
+    });
+
+    socket.on('add message', function (data) {
+        renderMessage(data);
+    });
+    socket.on('cooldown', async function (remaining, cooldown) {
+        if (!cooldown) return;
+        if (!remaining) {
+            cooldownArea.innerText = moment
+                .duration(cooldown, 'seconds')
+                .format('h:mm:ss', { trim: false });
+            let e = Number(cooldown);
+            var interval = setInterval(() => {
+                if (e == 0) {
+                    cooldownArea.innerText = 'El modo lento está activado';
+                    return clearInterval(interval);
+                }
+                e--;
+                cooldownArea.innerText = moment
+                    .duration(e, 'seconds')
+                    .format('h:mm:ss', { trim: false });
+            }, 1000);
+            return;
+        }
+        for (let i = 0; i < 3; i++) {
+            cooldownArea.style.color = '#FF2727';
+            await new Promise((s) => setTimeout(s, 100));
+            cooldownArea.style.color = '#FFFFFF';
+            await new Promise((s) => setTimeout(s, 100));
+        }
+    });
+
+    function clearInput(msg) {
+        return $('<div/>').text(msg).text();
     }
-    if (event.which === 13) {
-      addMessage()
+
+    function addMessageElement(el, options) {
+        var $el = $(el);
+
+        if (!options) {
+            options = {};
+        }
+        if (typeof options.fade === 'undefined') {
+            options.fade = true;
+        }
+        if (typeof options.prepend === 'undefined') {
+            options.prepend = false;
+        }
+
+        if (options.fade) {
+            $el.hide().fadeIn(120);
+        }
+        if (options.prepend) {
+            $messages.prepend($el);
+        } else {
+            $messages.append($el);
+        }
+
+        $messages[0].scrollTop = $messages[0].scrollHeight;
     }
-  });
-})
+    function addMessage() {
+        let addMgs = $inputMessage.val();
+        addMsg = clearInput(addMgs);
+        if (addMsg) {
+            $inputMessage.val('');
+            socket.emit('add message', key, addMsg);
+        }
+    }
+
+    function renderMessage(data, options) {
+        options = options || {};
+        let $messageDiv = '';
+        $imgAvatar = $(
+            `<img src="${data.avatarURL}" alt="${data.username}-avatar" class="circle">`
+        );
+        $divUserTwo = md.render(data.content);
+        $divUser = $(`<span class="title" style="color: ${data.userColor}">${
+            data.username
+        }</span>
+                ${md.render(data.content)}`);
+
+        if (lastMessage.id === data.id && lastMessage.author === data.author) {
+            $messageDiv = $(
+                `<li id=${data.messageID} class="collection-item item-chat" id="msg-el"/>`
+            ).append($divUserTwo);
+        } else {
+            lastMessage.id = data.id;
+            lastMessage.author = data.author;
+            $messageDiv = $(
+                `<li id=${data.messageID} class="collection-item avatar"/>`
+            ).append($imgAvatar, $divUser);
+        }
+
+        addMessageElement($messageDiv, options);
+    }
+
+    function msgTemplate(data) {
+        if (data.attachmentURL) {
+            $divUser = $(
+                `<span class="title" style="color: ${data.colorName}">${
+                    data.author
+                } <span class="datep">${data.date}</span></span><p>${
+                    data.content
+                }<${
+                    data.attachmentURL.endsWith('.mp4')
+                        ? 'video controls'
+                        : 'img'
+                } class="img-content" src=${data.attachmentURL} /></p>`
+            );
+            $divUserTwo = $(
+                `<p>${data.content}<img class="img-content" src=${data.attachmentURL} /></p>`
+            );
+        } else {
+            $divUser = $(
+                `<span class="title" style="color: ${data.colorName}">${data.author} <span class="datep">${data.date}</span></span><p>${data.content} </p>`
+            );
+            $divUserTwo = $(`<p>${data.content}</p>`);
+        }
+        $imgAvatar = $(
+            `<img src="${data.avatarURL}" alt="${data.author}-avatar" class="circle">`
+        );
+
+        if (lastMessage.id === data.id && lastMessage.author === data.author) {
+            $messageDiv = $(
+                `<li id=${data.messageID} class="collection-item item-chat" id="msg-el" />`
+            ).append($divUserTwo);
+        } else {
+            lastMessage.id = data.id;
+            lastMessage.author = data.author;
+            $messageDiv = $(
+                `<li id=${data.messageID} class="collection-item avatar"/>`
+            ).append($imgAvatar, $divUser);
+        }
+
+        addMessageElement($messageDiv);
+    }
+
+    $window.keydown(function (event) {
+        if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+            $inputMessage.focus();
+        }
+        if (event.which === 13) {
+            addMessage();
+        }
+    });
+});
